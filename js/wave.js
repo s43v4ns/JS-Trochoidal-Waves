2c2cb5e8dce36f7360b0ed2f9699e1145c5bde6a
TheBlackStig <th3.black.st1g@gmail.com>
2019-10-08 11:24:18 +0100

    Wire frame now works. There was a specific class in the three library

--- js/wave.js
+++ js/wave.js
@@ -1,6 +1,13 @@
 /* 	Renders a 3 ocean scapre using trochiodal wave simulation
  *	Uses the three.js library to acheive teh 3d graphics*/
 
+// Constants
+const PLANE_WIDTH = 5;
+const PLANE_HEIGHT = 5;
+const PLANE_DENSITY = 2;
+const PLANE_WIDTH_SEGMENTS = PLANE_WIDTH * PLANE_DENSITY;
+const PLANE_HEIGHT_SEGMENTS = PLANE_HEIGHT * PLANE_DENSITY;
+
 // Create the renderer object and apply it to the body of the page
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -14,20 +21,24 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 //var cameraOrbitControls = new THREE.OrbitControls(camera, renderer.domElement);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+/*var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+scene.add( cube );*/
+var planeGeometry = new THREE.PlaneGeometry(PLANE_WIDTH,
+											PLANE_HEIGHT,
+											PLANE_WIDTH_SEGMENTS,
+											PLANE_HEIGHT_SEGMENTS);
+var planeMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+planeMaterial.wireframe = true;
+var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+scene.add( plane );
 
-scene.add(createCoordinateMesh(5,5));
-
+console.log("here2")
 camera.position.z = 5;
 
 var animate = function () {
 	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 };
@@ -43,41 +54,4 @@ function currentCameraPosition(){
 
 function updateCameraOrbitPosition(x,y,z){
 	// Updates the orbital position of the camera
-}
-
-function createCoordinateMesh(width, length, density=1){
-	// Creates a mesh of connected coordinates
-	// Mesh consist of geometry and material
-	// Create the geometry
-	var geometry = new THREE.Geometry();
-
-	// Starting height
-	const Y_DEFAULT = 0;
-
-	// Starting x will be 0-(width/2)
-	var x = 0-(width/2);
-	var z = 0-(length/2);
-
-	// Loop through the number of points in the width first then the length
-	for(var i = 0; i < (width*density); i++){
-		for (var j = 0; j < (length*density); j++){
-			geometry.vertices.push(new THREE.Vector3(x,Y_DEFAULT,z));
-			// Increment the y axis each time
-			y += density;
-		}
-		// Increment the x axis each time
-		x += density;
-	}
-
-	// Compute the faces of the mesh
-
-	var generatedMesh = new THREE.Mesh(geometry);
-
-	return generatedMesh;
-}
-
-function calculateTriangles(inArray){
-	/*  Generates faces for the mesh given the vectors from the geometry using
-		Bowyer-Watson Algortihm for calculating Deluanay Triangulation */
-	
 }
