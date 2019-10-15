b7de7aff58bf835b8a5d1443c7009c92a2799fb5
TheBlackStig <th3.black.st1g@gmail.com>
2019-10-15 12:42:19 +0100

    Started work on groundwork for the seastate object

--- js/SeaSurface.js
+++ js/SeaSurface.js
@@ -4,6 +4,39 @@
     https://developer.nvidia.com/gpugems/GPUGems/gpugems_ch01.html
 */
 
-class SeaSurface{
+class SeaSurface extends THREE.Mesh{
+    // Represents a seasuraface and is based on the mesh object
+
+    // Object attributes
+    #steepness_coefficient;         // Range 0-1 decides how steep the waves aren
+    #wavelength;                    // length of a complete waves
+    #amplitude;                     // "Height" of the wave
+    #speed;                         // speed of the waveSteepnessMesh
+    #direction;                     // direction of the waves given as 2D vector
+    #phase_constant;                // Subsitute for speed (provides better cleaner equations)
+
+    constructor(geometry, material, wavelength, amplitude, speed, direction){
+        /*  Takes the inputs:
+            . geometry - THREE.Geometry -  to pass to the mesh constructor
+            . material - THREE.Material - to pass to the mesh constructor
+            . wavelength - float
+            . amplitude - float
+            . speed - float
+            . direction - THREE.Vector2
+            . steepness_coefficient - float (0-1)
+        */
+        // Instantiate the mesh superclass
+        super(geometry, material);
+
+        // Setup the initial variables
+        this.wavelength = wavelength;
+        this.amplitude = amplitude;
+        this.speed = speed;
+        this.direction = direction;
+    }
+
+    set steepness_coefficient(){}
+    set direction(){}
+
 
 }
